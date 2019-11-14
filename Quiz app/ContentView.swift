46f0c17991c0558ce1ca320e4a2d10c1cf509440
Team CLS <Mikelangballejensen@gmail.com>
2019-11-14 21:12:29 +0100

    test 2

---
+++ Quiz app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quiz app
+//
+//  Created by Simone Aagaard on 13/11/2019.
+//  Copyright © 2019 Team CLS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hi Mike")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
